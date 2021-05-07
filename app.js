52e19f3eb7737b89d82c1b259fd2f4b29951d4d5
Nadeem-Designer <nadeem2488@gmail.com>
2021-05-07 12:29:12 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// var $;
+// var jQuery;
+// console.log($, typeof $);
+// console.log(jQuery, typeof jQuery);
+
+// $("body").append("<h1>Hello World</h1>");
+// $("body").append("<h1>Hello Yusuf</h1>");
+
+// $(document).ready(function(){
+// 	$("body").append("<h1>Hello World</h1>");
+// });
+
+$(function(){
+	$("body").append("<h1>Hello World!!!</h1>");
+});
